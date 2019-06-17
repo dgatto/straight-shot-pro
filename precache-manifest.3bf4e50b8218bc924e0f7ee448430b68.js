8ee7db94f38cd7859efb6af6db86fdcdb01aa506
Donald Gatto <dgattoiii@gmail.com>
2019-06-17 19:32:13 -0400

    Updates

---
+++ precache-manifest.3bf4e50b8218bc924e0f7ee448430b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b5f08dd637932d7c3c4f9c22f10323",
+    "url": "/straight-shot-pro/index.html"
+  },
+  {
+    "revision": "749cc5746b3623cfd46f",
+    "url": "/straight-shot-pro/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/straight-shot-pro/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "749cc5746b3623cfd46f",
+    "url": "/straight-shot-pro/static/js/main.ff13fc7f.chunk.js"
+  },
+  {
+    "revision": "16401a7662b319eda135",
+    "url": "/straight-shot-pro/static/js/runtime~main.983e210b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/straight-shot-pro/static/media/logo.5d5d9eef.svg"
+  }
+]);
